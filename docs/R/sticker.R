e5cf46895944e26e017f50552dd10af6bd7b8544
witusj <wfth69@gmail.com>
2018-06-20 18:15:39 +0200

    New R code

--- docs/R/sticker.R
+++ docs/R/sticker.R
@@ -3,7 +3,7 @@ library(ggplot2)
 theme_set(theme_classic())
 
 # Plot
-g <- ggplot(mpg, aes(cty), height = 100)
+g <- ggplot(mpg, aes(cty), height = 10)
 g <- g + geom_density(aes(fill=factor(cyl)), alpha=0.8) +
   theme(axis.title.x=element_blank(),
         axis.text.x=element_blank(),
@@ -14,6 +14,6 @@ g <- g + geom_density(aes(fill=factor(cyl)), alpha=0.8) +
         legend.position="none"
         )
 
-sticker(g, package="OGBigD", p_size=5, s_x=1, s_y=.8, s_width=1.2, s_height=1,
-        h_fill="#f9690e", h_color="#f39c12",
+sticker(g, package="OGBigD", p_size=5, s_x=1, s_y=.8, s_width=1, s_height=0.8,
+        h_fill="#FFCC33", h_color="#FFFF66",
         filename="logo.png")
